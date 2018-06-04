394cbc3101a3985f1179fe5bfa3da75cb9f9784c
Alex Nault <nault.alex@gmail.com>
2018-06-04 14:13:09 -0400

    Add standards

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.ccss = require("./ccss.json");
+exports.florida = require("./florida.json");
+exports.missouri = require("./missouri.json");
+exports.ncss = require("./ncss.json");
+exports.newyork = require("./newyork.json");
+exports.pennsylvania = require("./pennsylvania.json");
+exports.ngss = require("./ngss.json");
+exports.texas = require("./texas.json");
