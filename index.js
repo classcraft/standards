da8ecdc88ab2ca868b947796c014426ef1338631
Alex Nault <nault.alex@gmail.com>
2018-06-05 10:43:33 -0400

    Update jurisdiction naming

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-exports.ccss = require("./ccss.json");
-exports.florida = require("./florida.json");
-exports.missouri = require("./missouri.json");
-exports.ncss = require("./ncss.json");
-exports.newyork = require("./newyork.json");
-exports.pennsylvania = require("./pennsylvania.json");
-exports.ngss = require("./ngss.json");
-exports.texas = require("./texas.json");
+exports["CCSS"] = require("./ccss.json");
+exports["Florida"] = require("./florida.json");
+exports["Missouri"] = require("./missouri.json");
+exports["NCSS"] = require("./ncss.json");
+exports["New York"] = require("./newyork.json");
+exports["Pennsylvania"] = require("./pennsylvania.json");
+exports["NGSS"] = require("./ngss.json");
+exports["Texas"] = require("./texas.json");
